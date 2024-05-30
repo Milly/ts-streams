51aa419ecf3049db42ac18f022aee4e7741ca60a
Milly <milly.ca@gmail.com>
2024-05-31 01:23:36 +0900

    feat: add `readable/from-message`

---
+++ readable/from_message.ts
@@ -0,0 +1,127 @@
+/** Message target type for {@linkcode fromMessage}. */
+// deno-lint-ignore no-explicit-any
+export interface FromMessageTarget<T = any> {
+  // deno-lint-ignore no-explicit-any
+  onmessage: ((evt: MessageEvent<T>) => any) | null;
+}
+
+/**
+ * Represents options for {@linkcode fromMessage}.
+ *
+ * @template T The message data type.
+ * @template R The return type of the `predicate`.
+ */
+export interface FromMessageOptions<T, R> {
+  /**
+   * If `true`, do not throw an error even if `target.onmessage` is not empty.
+   *
+   * @default {false}
+   */
+  force?: boolean;
+  /**
+   * A function that accepts up to two arguments. It is called one time for
+   * each message from the target.
+   *
+   * @param evt A message event object.
+   * @param index The index of the current message from the target.
+   * @returns A converted value.
+   */
+  predicate?(evt: MessageEvent<T>, index: number): R;
+}
+
+/**
+ * Creates a {@linkcode ReadableStream} that emits data from the given message
+ * target.
+ *
+ * If a `predicate` function is specified, its return value will be emitted
+ * in chunks. It is called synchronously from the `target.onmessage` handler.
+ *
+ * @example
+ * ```ts
+ * import { fromMessage } from "@milly/streams/readable/from-message";
+ * import { take } from "@milly/streams/transformer/take";
+ *
+ * const { port1, port2 } = new MessageChannel();
+ * const output = fromMessage(port1).pipeThrough(take(3));
+ * port2.postMessage(1);
+ * port2.postMessage("foo");
+ * port2.postMessage(true);
+ * const result = await Array.fromAsync(output);
+ * console.log(result); // [1, "foo", true]
+ * ```
+ *
+ * @template T The message data type.
+ * @template R The return type of the `predicate`.
+ * @param target The DOM EventTarget.
+ * @param options Option parameters object.
+ * @returns A ReadableStream that emits data from the given message target.
+ * @throws TypeError If `target.onmessage` is not empty and `options.force` is not `true`.
+ */
+export function fromMessage<T, R = T>(
+  target: FromMessageTarget<T>,
+  options?: FromMessageOptions<T, R>,
+): ReadableStream<R>;
+/**
+ * Creates a {@linkcode ReadableStream} that emits data from the given message
+ * target.
+ *
+ * If a `predicate` function is specified, its return value will be emitted
+ * in chunks. It is called synchronously from the `target.onmessage` handler.
+ *
+ * @example
+ * ```ts
+ * import { fromMessage } from "@milly/streams/readable/from-message";
+ * import { take } from "@milly/streams/transformer/take";
+ *
+ * const { port1, port2 } = new MessageChannel();
+ * const output = fromMessage(port1).pipeThrough(take(3));
+ * port2.postMessage(1);
+ * port2.postMessage("foo");
+ * port2.postMessage(true);
+ * const result = await Array.fromAsync(output);
+ * console.log(result); // [1, "foo", true]
+ * ```
+ *
+ * @template T The message data type.
+ * @template R The return type of the `predicate`.
+ * @param target The DOM EventTarget.
+ * @param predicate A function that accepts up to two arguments. It is called one time for each message from the target.
+ * @returns A ReadableStream that emits data from the given message target.
+ * @throws TypeError If `target.onmessage` is not empty.
+ */
+export function fromMessage<T, R>(
+  target: FromMessageTarget<T>,
+  predicate: Required<FromMessageOptions<T, R>>["predicate"],
+): ReadableStream<R>;
+export function fromMessage<T, R>(
+  target: FromMessageTarget<T>,
+  optionsOrPredicate?:
+    | FromMessageOptions<T, R>
+    | Required<FromMessageOptions<T, R>>["predicate"],
+): ReadableStream<T | R> {
+  const options = (typeof optionsOrPredicate === "function")
+    ? { predicate: optionsOrPredicate }
+    : optionsOrPredicate;
+  const { force, predicate } = options ?? {};
+  if (target.onmessage != null && !force) {
+    throw new TypeError("target.onmessage is not empty");
+  }
+  let index = 0;
+  return new ReadableStream({
+    start(controller) {
+      target.onmessage = (evt) => {
+        try {
+          ++index;
+          const chunk = predicate ? predicate(evt, index) : evt.data;
+          controller.enqueue(chunk);
+        } catch (e: unknown) {
+          target.onmessage = null;
+          controller.error(e);
+        }
+      };
+    },
+    cancel() {
+      target.onmessage = null;
+    },
+  });
+}
---
+++ readable/from_message_test.ts
@@ -0,0 +1,148 @@
+import { describe, it } from "@std/testing/bdd";
+import {
+  assertEquals,
+  assertInstanceOf,
+  assertThrows,
+  unimplemented,
+} from "@std/assert";
+import { assertType, type IsExact } from "@std/testing/types";
+import { assertSpyCalls, spy } from "@std/testing/mock";
+import { delay } from "@std/async/delay";
+import { testStream } from "@milly/streamtest";
+import { fromMessage, type FromMessageTarget } from "./from_message.ts";
+
+describe("fromMessage()", () => {
+  describe("returns a ReadableStream<T> type if", () => {
+    it("is no `predicate`", () => {
+      type X = { x: number };
+      const target: FromMessageTarget<X> = { onmessage: null };
+
+      const actual = fromMessage(target);
+
+      assertType<IsExact<typeof actual, ReadableStream<X>>>(true);
+      assertInstanceOf(actual, ReadableStream);
+    });
+    it("specifies message type", () => {
+      type X = { x: number };
+      const target: FromMessageTarget = { onmessage: null };
+
+      const actual = fromMessage<X>(target);
+
+      assertType<IsExact<typeof actual, ReadableStream<X>>>(true);
+      assertInstanceOf(actual, ReadableStream);
+    });
+    it("is `predicate` return type", () => {
+      type R = { x: number };
+      const target: FromMessageTarget = { onmessage: null };
+      const predicate = (): R => ({ x: 42 });
+
+      const actual = fromMessage(target, predicate);
+
+      assertType<IsExact<typeof actual, ReadableStream<R>>>(true);
+      assertInstanceOf(actual, ReadableStream);
+    });
+    it("is `options.predicate` return type", () => {
+      type R = { x: number };
+      const target: FromMessageTarget = { onmessage: null };
+      const predicate = (): R => ({ x: 42 });
+
+      const actual = fromMessage(target, { predicate });
+
+      assertType<IsExact<typeof actual, ReadableStream<R>>>(true);
+      assertInstanceOf(actual, ReadableStream);
+    });
+  });
+  describe("if `target.onmessage` is not empty", () => {
+    const target: FromMessageTarget<string> = {
+      onmessage: () => unimplemented(),
+    };
+
+    it("throws if `force` is not specified", () => {
+      assertThrows(
+        () => fromMessage(target),
+        TypeError,
+        "target.onmessage is not empty",
+      );
+    });
+    it("throws if `force` is false", () => {
+      assertThrows(
+        () => fromMessage(target, { force: false }),
+        TypeError,
+        "target.onmessage is not empty",
+      );
+    });
+    it("does not throws if `force` is true", () => {
+      fromMessage(target, { force: true });
+    });
+  });
+  describe("returns a ReadableStream and", () => {
+    it("set or delete the message handler to the `target`", async () => {
+      const target = { onmessage: null };
+
+      const actual = fromMessage(target);
+
+      assertInstanceOf(target.onmessage, Function);
+
+      await actual.cancel();
+
+      assertEquals(target.onmessage, null);
+    });
+    it("emits the message data from the `target`", async () => {
+      await testStream(async ({ assertReadable }) => {
+        const target: FromMessageTarget<string> = { onmessage: null };
+        (async () => {
+          await delay(100);
+          target.onmessage?.(new MessageEvent("message", { data: "a" }));
+          await delay(200);
+          target.onmessage?.(new MessageEvent("message", { data: "b" }));
+          await delay(300);
+          target.onmessage?.(new MessageEvent("message", { data: "c" }));
+        })();
+        const expected = "-a-b--c---";
+
+        const actual = fromMessage(target);
+
+        await assertReadable(actual, expected);
+      });
+    });
+    it("emits the return value of the `predicate`", async () => {
+      await testStream(async ({ assertReadable }) => {
+        const target: FromMessageTarget<string> = { onmessage: null };
+        (async () => {
+          await delay(100);
+          target.onmessage?.(new MessageEvent("message", { data: "a" }));
+          await delay(200);
+          target.onmessage?.(new MessageEvent("message", { data: "b" }));
+          await delay(300);
+          target.onmessage?.(new MessageEvent("message", { data: "c" }));
+        })();
+        const predicate = spy((evt: MessageEvent<string>) =>
+          evt.data.toUpperCase()
+        );
+        const expected = "-A-B--C---";
+
+        const actual = fromMessage(target, predicate);
+
+        await assertReadable(actual, expected);
+        assertSpyCalls(predicate, 3);
+      });
+    });
+    it("aborts if `predicate` throws an error", async () => {
+      await testStream(async ({ assertReadable }) => {
+        const target: FromMessageTarget<string> = { onmessage: null };
+        (async () => {
+          await delay(100);
+          target.onmessage?.(new MessageEvent("message", { data: "a" }));
+        })();
+        const predicate = () => {
+          throw "error";
+        };
+        const expected = "-#";
+
+        const actual = fromMessage(target, predicate);
+
+        await assertReadable(actual, expected, {}, "error");
+      });
+    });
+  });
+});
